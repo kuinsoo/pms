48af9455149592e2b1f07deac8fb84abeff8cc2a
najinsil <sally1334@naver.com>
2018-12-04 12:11:38 +0900

    나진실 @SessionAttributes commit.

--- src/main/java/kr/or/ddit/member/web/LoginController.java
+++ src/main/java/kr/or/ddit/member/web/LoginController.java
@@ -1,15 +1,20 @@
 package kr.or.ddit.member.web;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import kr.or.ddit.commons.mail.service.EmailSserviceInf;
 import kr.or.ddit.member.model.MemberVo;
 import kr.or.ddit.member.service.MemberServiceInf;
 
@@ -29,10 +34,17 @@ import kr.or.ddit.member.service.MemberServiceInf;
  *
  * </pre>
  */
+@SessionAttributes("memberVo")
 @Controller
 public class LoginController {
 	@Autowired
 	private MemberServiceInf memberservice;
+	
+	@Autowired
+	private EmailSserviceInf emailService;
+	
+	@Autowired
+	public JavaMailSender emailSender;
 	
 	/**
 	 * Method : index
@@ -41,7 +53,7 @@ public class LoginController {
 	 * @return
 	 * Method 설명 : index 로그인 화면으로 
 	 */
-	@RequestMapping(value="/")
+	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String index() {
 		return "/login/login";
 	}
@@ -52,25 +64,38 @@ public class LoginController {
 	 * 변경이력 :
 	 * @return
 	 * Method 설명 : 로그인 기능 
+	 * @throws UnsupportedEncodingException 
 	 */
+
 	@RequestMapping(value="/loginProcess",method=RequestMethod.POST)
-	public String loginProcess(HttpServletRequest request , Model model, HttpSession session) {
+	public String loginProcess(HttpServletRequest request, Model model, MemberVo memberVo, HttpSession session ) throws UnsupportedEncodingException {
+		request.setCharacterEncoding("utf-8");
 		
-		String member_mail = request.getParameter("member_mail");
-		String member_pass = request.getParameter("member_pass");
-		MemberVo memberVo = memberservice.selectUser(member_mail);
+		String member_mail = request.getParameter("member_mail").toLowerCase(); 
+		String member_pass = request.getParameter("member_pass").toLowerCase(); // 대소문자를 안가린다.		
+		
+		memberVo = memberservice.selectUser(member_mail);
+		int update_pass = memberservice.updatePass(member_pass);
 		
 		if(memberVo==null || !member_mail.equals(memberVo.getMember_mail())||
-							 !member_pass.equals(memberVo.getMember_pass())) {
+							 !member_pass.equals(memberVo.getMember_pass())){
 			
 			model.addAttribute("member_mail",member_mail);
 			model.addAttribute("member_pass",member_pass);
 			
-			return "login/login";
+			return "/login/login";
+		
 		}else {
-			session.setAttribute("memberVo",memberVo);
+			
+			model.addAttribute("memberVo",memberVo);
+			return "redirect:/main";
 		}
-		return "main/main";
+		
+	}
+	
+	@RequestMapping(value="/main",method=RequestMethod.GET)
+	public String main() {
+		return "/main/main";
 	}
 	
 	/**
@@ -81,9 +106,29 @@ public class LoginController {
 	 * Method 설명 : 아이디 찾기 
 	 */
 	@RequestMapping(value="/findEmail",method=RequestMethod.POST)
-	public String findEmail() {
+	public String findEmail(HttpServletRequest request, MemberVo memberVo,String to, String subject, String content){
 		
-		return "/login/login";
+		String member_name = request.getParameter("member_name");
+		// 자신의 진짜 메일 
+		String member_email = request.getParameter("member_email");
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		// 보낼사람 이메일
+		message.setTo(member_email);
+		// 제목
+		message.setSubject(member_name + "님 안녕하세요 :) < CURRENT 아이디 찾기 >");
+		
+		if(memberservice.selectfindId(memberVo)!=null) {
+			// 내용
+			message.setText(member_name + "님이 찾으시는 아이디는  [  " + memberservice.selectfindId(memberVo).getMember_mail() + "  ]  입니다.");
+			
+			emailSender.send(message);
+			
+			return "/login/login";
+		}else {
+			return "/login/login";
+		}
 	}
 	
 	
@@ -95,12 +140,41 @@ public class LoginController {
 	 * @return
 	 * Method 설명 : 비밀번호 찾기 
 	 */
-	@RequestMapping(value="/passFind",method=RequestMethod.POST)
-	public String passFind() {
+	@RequestMapping(value="/findPass",method=RequestMethod.POST)
+	public String findPass(HttpServletRequest request, MemberVo memberVo,Model model ,String to, String subject, String content) {
+		// 임의값 받아오는거 
+		String member_pass = "";
+		String member_name = request.getParameter("member_name");
+		String member_mail = request.getParameter("member_mail");
 		
-		return "/login/login";
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		// 아이디 찾기와 동일하게 보낼사람 이메일 
+		message.setTo(member_mail);
+		// 제목
+		message.setSubject(member_name + "님 안녕하세요 :) < CURRENT 비밀번호 찾기 >");
+		
+		// 임시 번호로 
+		// null이 아니면 비밀번호가 전송 
+		if(memberservice.selectfindPass(memberVo)!=null) {
+			// random 로직 
+			String possible ="abcdefghijklmnopqrstuvwxyz0123456789";
+			for(int i = 0; i<8; i++) {
+				member_pass += possible.charAt((int) Math.floor(Math.random()* possible.length()));
+			}	
+				message.setText(member_name+" 님의 임시 비밀번호는  [  " + member_pass + "  ]  입니다. 로그인 후 꼭! 비밀번호를 수정해 주세요. ");
+				// memberVo에 member_pass를 담아준다. 
+				memberVo.setMember_pass(member_pass);
+				
+				//
+				emailSender.send(message);
+				
+				
+				return "/login/login";
+		}else {
+			return "/login/login";
+		}
 	}
-	
 	
 	/**
 	 * Method : signView
@@ -134,6 +208,4 @@ public class LoginController {
 			return "/sign/sign";
 		}
 	}
-	
-	
 }
