f2372ba5799e0de8794d7d0f01d727c3ba8e01f2
KuInSoo <kkuinsoo@gmail.com>
2018-12-07 14:49:24 +0900

    Mr.kku : 12월 7일 11:21 1. 프로젝트 초대 2. DB Table 추가 Invite_Project

--- src/test/java/kr/or/ddit/project/web/ProjectControllerTest.java
+++ src/test/java/kr/or/ddit/project/web/ProjectControllerTest.java
@@ -1,25 +1,19 @@
 package kr.or.ddit.project.web;
 
 import kr.or.ddit.member.service.MemberService;
-import kr.or.ddit.project.model.ProjectVo;
 import kr.or.ddit.project.service.ProjectService;
-import kr.or.ddit.project.service.ProjectServiceInf;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 /**
