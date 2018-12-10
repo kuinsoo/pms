90d1ebc894e3bafaa5c28786933538a1177d4086
najinsil <sally1334@naver.com>
2018-12-10 16:50:39 +0900

    진실 commit

--- src/main/java/kr/or/ddit/member/web/MemberDetailController.java
+++ src/main/java/kr/or/ddit/member/web/MemberDetailController.java
@@ -2,6 +2,7 @@ package kr.or.ddit.member.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -80,7 +81,7 @@ public class MemberDetailController {
 			
 			try {
 				if(part.getSize()>0) {
-					String path = request.getServletContext().getRealPath("/profile");
+					String path = request.getServletContext().getRealPath("/images");
 					
 					
 					String fileName = part.getOriginalFilename();
@@ -88,7 +89,7 @@ public class MemberDetailController {
 					part.transferTo(new File(path + File.separator + fileName));
 								
 					// profile
-					memberVo.setMember_profile("/member_profile/"+fileName);
+					memberVo.setMember_profile("/images/"+fileName);
 				}
 			} catch (IllegalStateException e) {
 				e.printStackTrace();
@@ -97,18 +98,51 @@ public class MemberDetailController {
 			}
 				
 			int updateUser = memberservice.updateUser(memberVo);
-			System.out.println(" 수정  : " + memberservice.updateUser(memberVo));
 			model.addAttribute("memberVo",memberVo);
 			
-			System.out.println("수정 성공 ! ! ! ! ");
 			return "redirect:/myPage?member_mail=" + memberVo.getMember_mail();
 		}
 	
 	
+		/**
+		 * Method : myPageAjax
+		 * 작성자 : 나진실
+		 * 변경이력 :
+		 * @param request
+		 * @param model
+		 * @return
+		 * Method 설명 : myPage 휴대폰 인증 부분 
+		 */
 		@ResponseBody
 		@RequestMapping(value="/myPageAjax", method=RequestMethod.GET)
-		public String myPageAjax() {
-			return null;
+		public String myPageAjax(HttpServletRequest request, Model model) {
+			
+			// 핸드폰 번호 입력란 
+			String member_tel = request.getParameter("member_tel");
+		
+			String api_key = "NCSJQVBNAKBRXLTC";
+			String api_secret ="ZNJ2OS1W0F1A4N9FPRUKO8YXWT1RBXKR";
+			Coolsms coolsms = new Coolsms(api_key, api_secret);
+		
+			HashMap<String, String> set = new HashMap<String, String>();
+			set.put("to", member_tel); // 수신
+			set.put("from", "01099011334"); // 발신
+			
+			// random 로직 
+			String number ="0123456789";	
+			
+			// 인증번호 담아주는 변수 
+			String certificationNumber = "";
+			for(int i = 0; i < 4; i++) {
+				certificationNumber += number.charAt((int) Math.floor(Math.random()* number.length()));
+			}
+			set.put("text", "CURRENT 인증번호는  [ " + certificationNumber + " ]  입니다. ");
+			set.put("type", "sms"); // 문자타입
+			
+			//JSONObject result = coolsms.send(set);// 보내기&전송결과받기
+			coolsms.send(set);
+			
+			return certificationNumber;
 			
 		}
 	}
