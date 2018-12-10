6822311bef1d8fc1517e2d80f74ac6d34fbffa74
najinsil <sally1334@naver.com>
2018-12-10 12:44:13 +0900

    진실 commit

--- src/main/java/kr/or/ddit/member/web/MemberDetailController.java
+++ src/main/java/kr/or/ddit/member/web/MemberDetailController.java
@@ -1,5 +1,8 @@
 package kr.or.ddit.member.web;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -9,8 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.ddit.member.model.MemberVo;
 import kr.or.ddit.member.service.MemberServiceInf;
@@ -42,20 +46,44 @@ public class MemberDetailController {
 	public String memberDetail(Model model, @SessionAttribute("memberVo")MemberVo memberVo) {
 		
 		memberservice.selectUser(memberVo.getMember_mail());
-		
 		return "/member/detail";
-		
 	}
-	@RequestMapping(value="/memberDetailUpdate",method = RequestMethod.GET)
-	public String memberDetailUpdate(Model model, @SessionAttribute("memberVo")MemberVo memberVo , HttpServletRequest request) {
+	
+	@RequestMapping(value="/memberUpdate",method = RequestMethod.GET)
+	public String memberUpdate(Model model, @SessionAttribute("memberVo")MemberVo memberVo) {
 		
-		String member_tel = request.getParameter("member_tel");
-		String member_pass = request.getParameter("member_pass");
+		memberservice.selectUser(memberVo.getMember_mail());
+		model.addAttribute("memberVo",memberVo);
+		return "/member/update";
+	}
+	
+	
+	 
+	@RequestMapping(value="/memberDetailUpdate", method=RequestMethod.POST)
+	public String memberDetailUpdate(Model model, @SessionAttribute("memberVo")MemberVo memberVo,
+		@RequestPart("member_profile") MultipartFile part, HttpServletRequest request) {
 		
-		memberservice.updateUser(memberVo);
+		try {
+			if(part.getSize()>0) {
+				String path = request.getServletContext().getRealPath("/profile");
+				
+				String fileName = part.getOriginalFilename();
+				
+				part.transferTo(new File(path + File.separator + fileName));
+							
+				// profile
+				memberVo.setMember_profile("/member_profile/"+fileName);
+			}
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int updateUser = memberservice.updateUser(memberVo);
 		
-		return "/member/detail";
+		model.addAttribute("memberVo",memberVo);
 		
+		return "redirect:/memberDetail?member_mail="+memberVo.getMember_mail();
 	}
 }
 
--- src/main/webapp/WEB-INF/views/member/update.jsp
+++ src/main/webapp/WEB-INF/views/member/update.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,34 +11,27 @@
 	<div>
 		<div>	
 			<div>	
+				<form action="/memberDetailUpdate" method="post"  enctype="multipart/form-data">
 					<div>
 						<label> 프로필 사진 등록 </label>
 						<div>
-							<c:choose>
-								<c:when test="${memberVo.member_profile == null}">
-									<img src='/profile/noimage.png' />
-								</c:when>
-								<c:otherwise>
-									<img src="${memberVo.member_profile}" />
-								</c:otherwise>
-							</c:choose>
+							<img alt="" src="${memberVo.member_profile}"> 
+							<input type="file" id="profile" name="member_profile"/>
 						</div>
 					</div> 
 					<div>
 						<label>핸드폰 번호 수정</label>
 						<div>
-							<label>${memberVo.member_tel}</label>
+							<input name = "member_tel" value ='${memberVo.member_tel}'/>
 						</div>
 					</div>
 					<div>
 						<label>비밀번호 수정</label>
 						<div>
-							<label>${memberVo.member_pass}</label>
+							<input name ="member_pass"  value ='${memberVo.member_pass}'/>
 						</div>
-					</div>						
-					<form action=/memberUpdate method="get">
-						<input type="hidden" name ="member_mail"  value ='${memberVo.member_mail}'/>
 						<button type="submit"class="btn btn-default">수정</button>
+					</div>
 				</form>
 			</div>
 		</div>
