26f2790bc924a802bf478c94767b32b15c57980c
kuinsoo <kkuinsoo@gmail.com>
2018-12-14 22:39:31 +0900

    Mr.KKu (2018.12.14 22:39) test 조금 수정

--- src/test/resources/kr/or/ddit/config/db/dditInit.sql
+++ src/test/resources/kr/or/ddit/config/db/dditInit.sql
@@ -6,3 +6,8 @@ Delete from WORK;
 Insert into WORK values ('1','8','WORK_TEST1','WORK_TEST_CONTENT','1',5,to_date('18/12/08','yy/MM/dd'),to_date('18/12/20','yy/MM/dd'),30,'N','','Y','');
 Insert into WORK values ('2','10','WORK_TEST_PROJECT10','WORK_TEST_CONTENT10','1',5,to_date('18/12/08','yy/MM/dd'),to_date('18/12/20','yy/MM/dd'),30,'N','','Y','');
 
+-- comment table
+Delete from comments;
+insert into comments values (SEQ_COMMENTS.nextval, '1', '나진실', '구인수 팀장님 쵝오!', sysdate);
+insert into comments values (SEQ_COMMENTS.nextval, '1', '테스트맨', '구인수 팀장님 쵝오!!!', sysdate);
+
