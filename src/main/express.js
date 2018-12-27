baa3f7245a76185315a64a5a4640f55f275bdee9
normalstory <everpresent@hanmail.net>
2018-12-27 13:49:50 +0900

    1. 회의방 개설 시 디비 저장 2. 회의목록 subMain 출력 3. 회의록 디비 저장 4. 레이어 팝업 개발 중

--- src/main/express.js
+++ src/main/express.js
@@ -38,7 +38,9 @@ app.use(bodyParser.json());
 app.all('/*', function(req, res, next) {
 	console.log(" \n * app.all * \n  ");
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-methods", "GET,POST,PUT");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With,X-PINGOTHER, Content-Type");
+	res.header("Access-Control-Max-Age", "86400");
 	next();
 });
 
@@ -47,16 +49,17 @@ app.post('/test', function (req, res) {
 	console.log("\n * app.post * \n");
 	var userid = req.body.userid,
 	username = req.body.username,
-	chatMessage = req.body.chatMessage;
+	chatMessage = req.body.chatMessage,
+	roomid=req.body.roomid;
 
+	console.log(chatMessage);
 	console.log(userid);
 	console.log(username);
-	console.log(chatMessage);
+	console.log(roomid);
 	
-
 	// 연결 : 데이터베이스(오라클) 연결 
 	oracledb.getConnection({
-				user          : "current2",
+				user          : "current1",
 				password      : "java",
 				connectString : "119.203.240.166/orcl",
 			},
@@ -69,7 +72,63 @@ app.post('/test', function (req, res) {
 
 				//	연결.실행 검증(SELECT문) 
 				connection.execute(      
-					"insert into chatting values(seq_CHAT_ID.nextval||'chat', '2room', '"+username+"',sysdate,'"+chatMessage+"','"+userid+"')",
+					"insert into chatting values(seq_CHAT_ID.nextval||'chat', NULL ,'"+username+"',sysdate,'"+chatMessage+"','"+userid+"','"+roomid+"')",
+					function(err, result){
+						if (err) {
+							console.error(err.message);
+							doRelease(connection);
+							return;
+						}
+						doRelease(connection);
+						console.log("--성공--");
+					});
+				
+			});
+	function doRelease(connection){
+		connection.release(
+			function(err) {
+				if (err) {
+					console.error(err.message);
+				}
+			});
+		};
+});
+
+
+//*** 데이터 받기 
+app.post('/meetProjObj', function (req, res) {
+	console.log("\n * app.post 2 * \n");
+	var project_id = req.body.project_id,
+	projectTitle = req.body.projectTitle,
+	roomid = req.body.roomid,
+	roomTitle = req.body.roomTitle,
+	member_name = req.body.member_name,
+	member_mail = req.body.member_mail;
+
+	console.log(project_id);
+	console.log(projectTitle);
+	console.log(roomid);
+	console.log(roomTitle);
+	console.log(member_name);
+	console.log(member_mail);
+
+	// 연결 : 데이터베이스(오라클) 연결 
+	oracledb.getConnection({
+				user          : "current1",
+				password      : "java",
+				connectString : "119.203.240.166/orcl",
+			},
+			function(err, connection){
+				if (err) {
+					console.error(err.message);
+					return;
+				}
+				console.log("짝짝짝-오라클 DB에 정상적으로 연결되었습니다.");
+
+				//	연결.실행 검증(SELECT문)   + MEETING_CONTENT - 회의록 
+				connection.execute(      
+					"insert into meeting(MEETING_ID, MEETING_PROJECT, MEETING_TITLE, MEETING_OVERVIEW, MEETING_FOUNDER, MEETING_SDATE, MEETING_EDATE)  " +
+					"values('meet'||seq_MEETING_ID.nextval, '"+project_id+"','"+roomid+"','"+roomTitle+"','"+member_mail+"',sysdate, sysdate+((1/1440)*30) )",
 					function(err, result){
 						if (err) {
 							console.error(err.message);
@@ -88,6 +147,52 @@ app.post('/test', function (req, res) {
 				}
 			});
 		};
-
 });
 
+
+//*** 데이터 받기 
+app.post('/saveDoc', function (req, res) {
+	console.log("\n * app.post 3 * \n");
+	var content = req.body.content,
+	project_id = req.body.project_id,
+	roomid = req.body.roomid;
+
+	console.log(project_id);
+	console.log(content);
+	console.log(roomid);
+
+	// 연결 : 데이터베이스(오라클) 연결 
+	oracledb.getConnection({
+				user          : "current1",
+				password      : "java",
+				connectString : "119.203.240.166/orcl",
+			},
+			function(err, connection){
+				if (err) {
+					console.error(err.message);
+					return;
+				}
+				console.log("짝짝짝-오라클 DB에 정상적으로 연결되었습니다.");
+
+				//	연결.실행 검증(SELECT문)   + MEETING_CONTENT - 회의록 
+				connection.execute(      
+					"update meeting set MEETING_CONTENT='"+content+"' where MEETING_PROJECT='"+project_id+"' and MEETING_TITLE='"+roomid+"'",
+					function(err, result){
+						if (err) {
+							console.error(err.message);
+							doRelease(connection);
+							return;
+						}
+						doRelease(connection);
+						console.log("--성공--");
+					});
+			});
+	function doRelease(connection){
+		connection.release(
+			function(err) {
+				if (err) {
+					console.error(err.message);
+				}
+			});
+		};
+});
--- src/main/java/kr/or/ddit/meeting/web/MeetingController.java
+++ src/main/java/kr/or/ddit/meeting/web/MeetingController.java
@@ -1,6 +1,21 @@
 package kr.or.ddit.meeting.web;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.or.ddit.meeting.model.MeetingVo;
+import kr.or.ddit.meeting.service.MeetingServiceInf;
+import kr.or.ddit.qna.web.QnAController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * kr.or.ddit.meeting.web
@@ -13,4 +28,20 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class MeetingController {
+
+	@Autowired
+	private MeetingServiceInf meetingService;
+	
+	Logger logger = LoggerFactory.getLogger(QnAController.class);
+	
+	//@GetMapping(value= "/meetingList")
+	@RequestMapping(value= "/meetingList", method= {RequestMethod.GET,RequestMethod.POST}) // headers="Accept=application/json"
+	public String meetingListView(@RequestParam("project_id")String project_id, Model model) {
+		
+		List<MeetingVo> meetingList= meetingService.meetingList(project_id);
+		model.addAttribute("meetingList", meetingList );
+		
+		return "main/meetListAjax";
+	}
+	
 }
