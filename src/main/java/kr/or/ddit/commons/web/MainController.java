e7dae9f831733fa4265c4d05f3b1355e2998168f
najinsil <sally1334@naver.com>
2018-12-31 12:29:27 +0900

    mainController 오류 수정 나진실

--- src/main/java/kr/or/ddit/commons/web/MainController.java
+++ src/main/java/kr/or/ddit/commons/web/MainController.java
@@ -1,21 +1,23 @@
 package kr.or.ddit.commons.web;
 
-import kr.or.ddit.member.model.MemberVo;
-import kr.or.ddit.member.service.MemberServiceInf;
-import kr.or.ddit.post.service.PostServiceInf;
-import kr.or.ddit.message.model.MessageVo;
-import kr.or.ddit.message.service.MessageServiceInf;
-import kr.or.ddit.project.service.ProjectServiceInf;
-import kr.or.ddit.util.model.PageVo;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import kr.or.ddit.member.model.MemberVo;
+import kr.or.ddit.member.service.MemberServiceInf;
+import kr.or.ddit.message.model.MessageVo;
+import kr.or.ddit.message.service.MessageServiceInf;
+import kr.or.ddit.post.service.PostServiceInf;
+import kr.or.ddit.project.service.ProjectServiceInf;
 
 @Controller
 public class MainController {
@@ -27,10 +29,12 @@ public class MainController {
 	private ProjectServiceInf projectService;
 	
 	@Autowired
-	private PostServiceInf postService;	
+	private PostServiceInf postService;
+	
 	
 	@Autowired
 	private MessageServiceInf messageService;
+	
 
 	/**
 	 * Main string.
@@ -65,7 +69,7 @@ public class MainController {
 		MessageVo messageVo = new MessageVo();
 		messageVo.setMsg_rmember(memberVo.getMember_mail());
 		
-		int totalmessageY = messageservice.totalmessageSizeCheck(memberVo.getMember_mail());
+		int totalmessageY = messageService.totalmessageSizeCheck(memberVo.getMember_mail());
 		
 		Map<String, Object> msgReceiveMap = new HashMap<>();
 		msgReceiveMap.put("totalmessageY", totalmessageY);
@@ -75,7 +79,6 @@ public class MainController {
 	
 	@RequestMapping(value="/inviteProject", method=RequestMethod.GET)
 	public String inviteProject(Model model, @SessionAttribute("memberVo")MemberVo memberVo, @RequestParam("project_id")String project_id) {
-		
 		Map<String, String > inviteMap = new HashMap<>();
 		model.addAttribute("project_id", project_id);
 		inviteMap.put("project_id",project_id);
