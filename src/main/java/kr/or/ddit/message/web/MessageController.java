b12e4680775d7c8ea43673861a6430a2fc6908b4
najinsil <sally1334@naver.com>
2019-01-03 17:21:42 +0900

    나진실 commit

--- src/main/java/kr/or/ddit/message/web/MessageController.java
+++ src/main/java/kr/or/ddit/message/web/MessageController.java
@@ -43,7 +43,7 @@ public class MessageController {
 	private FriendsListServiceInf friendservice;
 	
 	@Autowired
-	private MessageServiceInf messageService;
+	private MessageServiceInf messageservice;
 	
 	@Autowired
 	private PostServiceInf postService;
@@ -73,7 +73,7 @@ public class MessageController {
 		/* 알림기능 - IKS */
 		model.addAttribute("pageCnt", postService.totalPostCnt());
 		model.addAttribute("workMemberTotalCnt", workService.workMemberTotalCnt(memberVo.getMember_mail()));
-		model.addAttribute("totalMsgReceived", messageService.totalMsgReceived(memberVo.getMember_mail()));
+		model.addAttribute("totalMsgReceived", messageservice.totalMsgReceived(memberVo.getMember_mail()));
 		
 		
 		return "message/message";
